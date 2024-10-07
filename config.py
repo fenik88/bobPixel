fe2e22b53aca3cfef80029fc3585413c2b698f2a
fenik88 <46868162+fenik88@users.noreply.github.com>
2024-10-07 21:00:08 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+# put yours
+API_ID = 16125394
+API_HASH = "1e8b0b181295d5c62f02fc589462fb96"
+
+# =============[upgrades]================
+# default is 5 level you can change it your self
+PAINT_REWARD_MAX = 5 # max is 7
+ENERGY_LIMIT_MAX = 5 # max is 6
+RE_CHARGE_SPEED_MAX = 5 # max is 11
+
+# ================[proxy]================
+USE_PROXY = False # or put True if you want use it
+PROXIES = {
+    "http":"socks5://127.0.0.1",
+    "https":"socks5://127.0.0.1", # if you using socks4 or http only replace it with "socks5"
+}
